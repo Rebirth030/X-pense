3e9d43cd5838ec1ff6cf9c147023046c254c95cb
Iulia Maria Ghinea <ighinea@stud.hs-heilbronn.de>
2024-04-15 15:01:55 +0200

    Navigation without beauty added

--- Xpense_App/app/src/main/java/com/example/xpense_app/navigation/NavGraph.kt
+++ Xpense_App/app/src/main/java/com/example/xpense_app/navigation/NavGraph.kt
@@ -1,21 +1,161 @@
 package com.example.xpense_app.navigation
 
+import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.xpense_app.login.CreateRegister
+import com.example.xpense_app.login.LoginForm
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun navGraph(context: Context) {
-    var navController = rememberNavController()
-    var navHost = NavHost(navController, startDestination = NavigationItem.Login.route, builder = {
-        composable(NavigationItem.Login.route) {
-            com.example.xpense_app.login.LoginForm()
-        }
-        composable(NavigationItem.Register.route) {
-            com.example.xpense_app.login.CreateRegister()
-        }
+    val navController = rememberNavController()
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-    })
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = {
+            ModalDrawerSheet {
+                // Drawer content goes here
+                NavigationDrawerItem(
+                    label = { Text(text = "Login") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(NavigationItem.Login.route) {
+                            popUpTo(0)
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Register") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(NavigationItem.Register.route) {
+                            popUpTo(0)
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Timer") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(NavigationItem.Home.route) {
+                            popUpTo(0)
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Profiles") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(NavigationItem.Profiles.route) {
+                            popUpTo(0)
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Manual Booking") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(NavigationItem.Manual.route) {
+                            popUpTo(0)
+                        }
+                    }
+                )
+            }
+        }
+    ) {
+        Scaffold(topBar = {
+            TopAppBar(title = { Text(text = "") }, navigationIcon = {
+                IconButton(onClick = { coroutineScope.launch { drawerState.open() } }) {
+                        Icon(Icons.Rounded.Menu, contentDescription = "Menu", modifier = Modifier.padding(horizontal = 8.dp))
+                    }
+            })
+        }) {
+            NavHost(navController = navController, startDestination = NavigationItem.Login.route) {
+                composable(NavigationItem.Login.route) { LoginForm() }
+                composable(NavigationItem.Register.route) { CreateRegister() }
+                composable(NavigationItem.Home.route) { Timer() }
+                composable(NavigationItem.Profiles.route) { Profiles() }
+                composable(NavigationItem.Manual.route) { Manual() }
+            }
+        }
+    }
+}
+
+@Composable
+fun Timer(){
+    Box(modifier = Modifier
+        .fillMaxSize()){
+        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
+            verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Timer")
+        }
+    }
+}
+
+@Composable
+fun Profiles(){
+    Box(modifier = Modifier
+        .fillMaxSize()){
+        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
+            verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Profiles")
+        }
+    }
+}
+
+@Composable
+fun Manual(){
+    Box(modifier = Modifier
+        .fillMaxSize()){
+        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
+            verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Manual Booking")
+        }
+    }
 }
