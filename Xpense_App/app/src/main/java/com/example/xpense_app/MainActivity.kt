6700b3c490c561492b10345cb8c2bef87bf67b90
NiclasCode <nicmarcgro@gmail.com>
2024-04-16 17:06:21 +0200

    Updated Navgraph

--- Xpense_App/app/src/main/java/com/example/xpense_app/MainActivity.kt
+++ Xpense_App/app/src/main/java/com/example/xpense_app/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.xpense_app.navigation.navGraph
+import com.example.xpense_app.navigation.NavGraph
 import com.example.xpense_app.ui.theme.XPense_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    navGraph(applicationContext)
+                    NavGraph(applicationContext)
                 }
             }
         }
--- Xpense_App/app/src/main/java/com/example/xpense_app/navigation/NavGraph.kt
+++ Xpense_App/app/src/main/java/com/example/xpense_app/navigation/NavGraph.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,17 +26,19 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.xpense_app.login.CreateRegister
 import com.example.xpense_app.login.LoginForm
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun navGraph(context: Context) {
+fun NavGraph(context: Context) {
     val navController = rememberNavController()
     val coroutineScope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
@@ -46,66 +49,11 @@ fun navGraph(context: Context) {
         drawerContent = {
             ModalDrawerSheet {
                 // Drawer content goes here
-                NavigationDrawerItem(
-                    label = { Text(text = "Login") },
-                    selected = false,
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(NavigationItem.Login.route) {
-                            popUpTo(0)
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text(text = "Register") },
-                    selected = false,
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(NavigationItem.Register.route) {
-                            popUpTo(0)
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text(text = "Timer") },
-                    selected = false,
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(NavigationItem.Home.route) {
-                            popUpTo(0)
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text(text = "Profiles") },
-                    selected = false,
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(NavigationItem.Profiles.route) {
-                            popUpTo(0)
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text(text = "Manual Booking") },
-                    selected = false,
-                    onClick = {
-                        coroutineScope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(NavigationItem.Manual.route) {
-                            popUpTo(0)
-                        }
-                    }
-                )
+                CreateNavigationItem("Login", coroutineScope, drawerState, navController, NavigationItem.Login)
+                CreateNavigationItem("Register", coroutineScope, drawerState, navController, NavigationItem.Register)
+                CreateNavigationItem("Timer", coroutineScope, drawerState, navController, NavigationItem.Home)
+                CreateNavigationItem("Profiles", coroutineScope, drawerState, navController, NavigationItem.Profiles)
+                CreateNavigationItem("Manual Booking", coroutineScope, drawerState, navController, NavigationItem.Manual)
             }
         }
     ) {
@@ -125,6 +73,28 @@ fun navGraph(context: Context) {
             }
         }
     }
+}
+
+@Composable
+private fun CreateNavigationItem(
+    text: String,
+    coroutineScope: CoroutineScope,
+    drawerState: DrawerState,
+    navController: NavHostController,
+    navRoute: NavigationItem
+) {
+    NavigationDrawerItem(
+        label = { Text(text = text) },
+        selected = false,
+        onClick = {
+            coroutineScope.launch {
+                drawerState.close()
+            }
+            navController.navigate(navRoute.route) {
+                popUpTo(0)
+            }
+        }
+    )
 }
 
 @Composable
